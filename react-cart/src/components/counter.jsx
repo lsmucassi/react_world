1765714a7fdd76e7867d1e480102b2b73fbb55fd
lsmucassi <lindasmucassi@gmail.com>
2021-03-29 21:46:48 +0200

    world of react - attr

---
+++ react-cart/src/components/counter.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+    state = {
+        count: 0,
+    }
+
+    styles = {
+        fontSize: 15,
+        fontWeight: 'bold'
+    }
+
+    render() {
+        return (
+        <div>
+            <span style={this.styles} className="badge badge-primary m-2" >{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
+        </div>
+        );
+    }
+
+    formatCount() {
+        const { count } = this. state 
+        return count === 0 ? 'Zero' : count
+    }
+}
+
+export default Counter;
