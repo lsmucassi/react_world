7d36e2980c15bea691cf805c4e481bca3c3e68e2
lsmucassi <lindasmucassi@gmail.com>
2021-03-29 22:58:52 +0200

    world of react - attr

--- react-cart/src/components/counter.jsx
+++ react-cart/src/components/counter.jsx
@@ -1,28 +1,50 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        count: 0,
-    }
+  state = {
+    count: 0,
+  };
 
-    styles = {
-        fontSize: 15,
-        fontWeight: 'bold'
-    }
+  styles = {
+    fontSize: 15,
+    fontWeight: "bold",
+  };
 
-    render() {
-        return (
-        <div>
-            <span style={this.styles} className="badge badge-primary m-2" >{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
-        </div>
-        );
-    }
+//   constructor() {
+//     super();
+//     this.handleIncrement = this.handleIncrement.bind(this)
+//   }
 
-    formatCount() {
-        const { count } = this. state 
-        return count === 0 ? 'Zero' : count
-    }
+  handleIncrement = () => {
+    console.log("Increment pressed");
+  }
+
+  render() {
+    return (
+      <div>
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
 
 export default Counter;
